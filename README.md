c39af647325d49c07487ebaff854e1e873bf5459
ansary29 <85570638+ansary29@users.noreply.github.com>
2022-03-06 21:29:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+ReactJS - NodeJS - MongoDB
+
+1) Login Authorizations.
+2) Admin Access.
+3) Customers booked Rooms.
+4) Online Payment (Stripe payment gateway)
+
+
+
---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+var mongoURL = "mongodb+srv://ansary29:moham29@myproject.12hak.mongodb.net/mernhotelbook?retryWrites=true&w=majority";
+
+mongoose.connect(mongoURL, {useUnifiedTopology : true, useNewUrlParser : true})
+    
+var connection = mongoose.connection;
+    
+connection.on('error', () => {  
+    console.log("MongoDB Connection Failed");
+})
+
+connection.on('connected', () => {
+    console.log("MongoDB Connected Successfully");
+})
+
+module.exports = mongoose
